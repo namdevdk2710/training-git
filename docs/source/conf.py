fcbb2c2d2f284792f083028ee330b2d915cfc771
Nam Dev <44866003+namdevdk2710@users.noreply.github.com>
2022-04-05 08:24:48 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Training GIT'
+copyright = '2021, NamDev'
+author = 'Nam Dev'
 
 release = '0.1'
 version = '0.1.0'
